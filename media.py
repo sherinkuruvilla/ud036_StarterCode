48a8029b1c1086f6d96edb0cd5b180c20498394a
sherinkuruvilla <sherin.kuruvilla@gmail.com>
2018-01-22 20:23:45 -0500

    Added comments as per pep8

--- media.py
+++ media.py
@@ -7,13 +7,20 @@ class Movie:
        The Movie class defines an object that can store details about movies"""
 
     def __init__(self, movie_title, movie_story_line, movie_poster_image, movie_youtube_trailer):
+        # This is the class initializing function for Movie class
+        # which requires as input
+        # title, description, image and trailer video urls values
+        # which are assigned to each instance.
         self.title = movie_title
         self.story_line = movie_story_line
         self.poster_image_url = movie_poster_image
         self.trailer_youtube_url = movie_youtube_trailer
 
     def show_trailer(self):
+        # Uses the web browser object to open the video of the attached url
         webbrowser.open(self.youtube_trailer)
 
     def show_poster(self):
+        # uses the web brower's open method
+        # to display the poster image
         webbrowser.open(self.poster_image)
